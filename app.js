28ce6c835962de55b7bffcb78374106f9c1628b9
Naveen Basireddy <naveenkumar.basireddy@gmail.com>
2021-10-30 23:13:14 +0530

    Customized Cursor

--- app.js
+++ app.js
@@ -42,3 +42,18 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle("toggle");
 });
 
+
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500)
+})
